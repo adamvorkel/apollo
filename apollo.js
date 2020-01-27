f53072af775873e4a751d663382ac3112b825c03
Adam <adamvorkel@gmail.com>
2020-01-27 21:52:59 +0200

    basic webserver working, serving static index.html from public dir

--- apollo.js
+++ apollo.js
@@ -3,8 +3,11 @@ const util = require('./core/util');
 const config = util.getConfig();
 const pipeline = require('./core/pipeline');
 
+if(config.launchUI) {
+    let ui = require('./web/server');
+}
+
 let instance = new pipeline(config);
 
-if(config.launchUI) {
-    //require(path.join(util.dirs().web, 'server'))
-}
+
+
--- web/server.js
+++ web/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.listen(port, () => {
     console.log(`Apollo UI listening on port ${port}`);
-})
+});
+
+return app;
