21516e62f55eb86f84f367a4f14137c08ba80410
Adam Vorkel <adamvorkel@gmail.com>
2020-02-12 18:16:34 +0200

    reworked the server api and project entry file a bit

--- bot/apollo.js
+++ bot/apollo.js
@@ -1,2 +1,62 @@
-let api = require('./server');
-return api;
+const server = require('./server');
+const BotManager = require('./BotManager');
+const manager = new BotManager();
+
+// Create a bot instance for dev/testing
+// remove later
+
+const api = server(manager);
+
+setTimeout(() => {
+    let firstBotConfig  = {
+        launchUI: true,
+        mode: "realtime",
+        watch: {
+            exchange: "binance",
+            currency: "USDT",
+            asset: "BTC",
+            tickrate: 10,
+            key: "gB8NBOByVIqXuAJxbPr266pPgpmJh4bAJz3UXg9ttBUNwde1Zt5K5kCgsd8u5193",
+            secret: "YYEjznijxEqaGemsEudwIxmGVQZpI4q7XkrXD0lzL4g21djgltZmvdcyqJW4bi73"
+        },
+        advisor: {
+            candleSize: "1m",
+            strategy: {
+                name: 'myStrategy',
+                params: {
+                    requiredHistory: 5
+                }
+            }
+        },
+        
+    };
+    
+    manager.createBot(firstBotConfig);
+}, 0);
+
+setTimeout(() => {
+    let secondBotConfig  = {
+        launchUI: true,
+        mode: "realtime",
+        watch: {
+            exchange: "binance",
+            currency: "BTC",
+            asset: "BNB",
+            tickrate: 10,
+            key: "gB8NBOByVIqXuAJxbPr266pPgpmJh4bAJz3UXg9ttBUNwde1Zt5K5kCgsd8u5193",
+            secret: "YYEjznijxEqaGemsEudwIxmGVQZpI4q7XkrXD0lzL4g21djgltZmvdcyqJW4bi73"
+        },
+        advisor: {
+            candleSize: "1m",
+            strategy: {
+                name: 'myStrategy',
+                params: [
+                    
+                ]
+            }
+        },
+    };
+    
+    manager.createBot(secondBotConfig);
+}, 2000);
+
--- bot/server.js
+++ bot/server.js
@@ -1,146 +1,75 @@
 const express = require('express');
-const BotManager = require('./BotManager');
 const WebSocketServer = require('ws').Server;
 
-const manager = new BotManager();
+let api = (botManager) => {
 
-// Create a bot instance for dev/testing
-// remove later
+    // return new Promise((res, rej) => {
 
-setTimeout(() => {
-    let firstBotConfig  = {
-        launchUI: true,
-        mode: "realtime",
-        watch: {
-            exchange: "binance",
-            currency: "USDT",
-            asset: "BTC",
-            tickrate: 10,
-            key: "gB8NBOByVIqXuAJxbPr266pPgpmJh4bAJz3UXg9ttBUNwde1Zt5K5kCgsd8u5193",
-            secret: "YYEjznijxEqaGemsEudwIxmGVQZpI4q7XkrXD0lzL4g21djgltZmvdcyqJW4bi73"
-        },
-        advisor: {
-            candleSize: "1m",
-            strategy: {
-                name: 'myStrategy',
-                params: {
-                    requiredHistory: 5
-                }
-            }
-        },
-        
-    };
-    
-    manager.createBot(firstBotConfig);
-}, 0);
+    // })
 
-setTimeout(() => {
-    let secondBotConfig  = {
-        launchUI: true,
-        mode: "realtime",
-        watch: {
-            exchange: "binance",
-            currency: "BTC",
-            asset: "BNB",
-            tickrate: 10,
-            key: "gB8NBOByVIqXuAJxbPr266pPgpmJh4bAJz3UXg9ttBUNwde1Zt5K5kCgsd8u5193",
-            secret: "YYEjznijxEqaGemsEudwIxmGVQZpI4q7XkrXD0lzL4g21djgltZmvdcyqJW4bi73"
-        },
-        advisor: {
-            candleSize: "1m",
-            strategy: {
-                name: 'myStrategy',
-                params: [
-                    
-                ]
-            }
-        },
-    };
-    
-    manager.createBot(secondBotConfig);
-}, 6000);
+    /*-----------------*/
+    // REST API server
+    /*-----------------*/
+    const app = express();
+    const port = 3001;
 
+    // App middleware config
+    app.use(express.json());
+    app.use(express.urlencoded({extended: true}));
 
-/*-----------------*/
-// REST API server
-/*-----------------*/
-const app = express();
-const port = 3001;
+    // Endpoints
+    app.get('/api/bots', (req, res) => {});
 
-// app middleware config
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+    app.post('/api/stopBot', (req, res) => {});
 
-// Endpoints
+    let server = app.listen(port, () => {
+        console.log(`Apollo API listening on port ${port}`);
+    });
 
-app.get('/api/bots', (req, res) => {
-    res.json(manager.listBots())
-});
+    /*-----------------*/
+    // WSS server
+    /*-----------------*/
+    const wss = new WebSocketServer({server: server});
 
-app.post('/api/stopBot', (req, res) => {
-    const id = req.body.id;
-    if(!id) {
-        res.json({status:"failure"});
-    } else {
-        //we have an id, stop bot instance
-        const stopped = manager.stopBot(id);
-        if(stopped) {
-            res.json({id: id, status:"success"});
-        } else {
-            res.json({status:"failure"});
-        }
-    }
-});
-
-let server = app.listen(port, () => {
-    console.log(`Apollo API listening on port ${port}`);
-});
-
-/*-----------------*/
-// WSS server
-/*-----------------*/
-
-const wss = new WebSocketServer({server: server});
-
-wss.on('connection', ws => {
-    console.log(`New websocket connected`)
-    ws.isAlive = true;
-    ws.on('pong', () => {
+    wss.on('connection', ws => {
+        console.log(`New websocket connected`)
         ws.isAlive = true;
-    });
-    ws.ping();
-    ws.on('error', e => {
-        // handle socket error
-    })
-});
-
-setInterval(() => {
-    wss.clients.forEach(ws => {
-        if(!ws.isAlive) {
-            return ws.terminate();
-        }
-        ws.isAlive = false;
+        ws.on('pong', () => {
+            ws.isAlive = true;
+        });
         ws.ping();
-    })
-}, 10 * 1000);
-
-const broadcast = (event, data) => {
-    const payload = JSON.stringify({
-        event: event, 
-        payload: data
-    });
-    wss.clients.forEach(ws => {
-        ws.send(payload, err => {
-            if(err) {
-                console.log("Broadcast error")
-            }
+        ws.on('error', e => {
+            // handle socket error
         })
-    })
+    });
+
+    setInterval(() => {
+        wss.clients.forEach(ws => {
+            if(!ws.isAlive) {
+                return ws.terminate();
+            }
+            ws.isAlive = false;
+            ws.ping();
+        })
+    }, 10 * 1000);
+
+    const broadcast = (event, data) => {
+        const payload = JSON.stringify({
+            event: event, 
+            payload: data
+        });
+        wss.clients.forEach(ws => {
+            ws.send(payload, err => {
+                if(err) {
+                    console.log("Broadcast error")
+                }
+            })
+        })
+    }
+
+    return {
+        broadcast
+    }
 }
 
-manager.on('botStarted', (payload) => {
-    broadcast('botStarted', payload);
-});
-
-
-return app;
+module.exports = api;
