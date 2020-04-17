638de4aa902ab14444bbc879baa0b1aeba257a18
Adam Vorkel <adamvorkel@gmail.com>
2020-04-17 11:14:34 +0200

    backend - history timestamp calc for candle requests

--- bot/markets/live.js
+++ bot/markets/live.js
@@ -32,4 +32,39 @@ class Live {
     // }
 }
 
+const Binance = require('../exchanges/wrappers/binance').api;
+const exchange = new Binance({
+    watch: {
+        key: "gB8NBOByVIqXuAJxbPr266pPgpmJh4bAJz3UXg9ttBUNwde1Zt5K5kCgsd8u5193",
+        secret: "YYEjznijxEqaGemsEudwIxmGVQZpI4q7XkrXD0lzL4g21djgltZmvdcyqJW4bi73",
+    }
+});
+
+const market = new Live(exchange);
+
+let ts = Date.now();
+let candleInterval = 5;
+let requiredHistory = 5;
+
+let endTime = new Date(ts);
+let minElapsedSinceHourStart = endTime.getMinutes();
+let lastCandleStartMin = Math.floor(minElapsedSinceHourStart / candleInterval) * candleInterval;
+endTime.setMinutes(lastCandleStartMin, 0, 0); // start of hour
+
+console.log(endTime.toLocaleString())
+
+let history = requiredHistory * candleInterval * 60 * 1000;
+let startTimeTS = endTime.getTime() - history;
+let startTime = new Date(startTimeTS);
+
+console.log(startTime.toLocaleString())
+
+
+// let  = startTime.setSeconds(0, 0);
+// console.log(startTime.toLocaleString());
+
+// console.log(startTime.toLocaleString());
+
+// market.exchange.klines({symbol: 'EOSBTC', interval: '5m'});
+
 module.exports = Live;
